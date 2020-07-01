9d8c1beb6f65cfb1687e586337d7f4fb7b13dd7e
Ivan Matveev <ivan.matveev@hs-anhalt.de>
2020-07-01 15:25:47 +0200

    Object plotting added.

--- main.py
+++ main.py
@@ -27,6 +27,30 @@ def scale_intrinsic(new_res, base_res, intrinsic):
     return intrinsic
 
 
+def parse_3d_obj_file(path):
+    """
+    Convert vertices to np.array([N, 4]) in homogeneous form.
+    Faces are converted to np.array([M, 3]), therefore faces must be preliminary triangulated
+    :param path: path to wavefront.obj file
+    :return: np.array(vertices), np.array(faces)
+    """
+    def parse_string(string):
+        spl = [el.split('//') for el in string.split()]
+        res = [el[0] for i, el in enumerate(spl) if i != 0]
+        return res
+
+    with open(path, "r") as fi:
+        lines = fi.readlines()
+
+    vertices = np.array([parse_string(ln) for ln in lines if ln.startswith("v")], dtype='float')
+    faces = [parse_string(ln) for ln in lines if ln.startswith("f")]
+    faces = np.asarray([[int(el) for el in ln] for ln in faces]) - 1
+
+    vertices = np.hstack((vertices, np.ones((vertices.shape[0], 1))))  # Bring to homogeneous form
+
+    return vertices, faces
+
+
 # sc_name = 'lamp_pole_1'
 sc_name = 'lamp_pole_opt'
 # sc_name = 'scene_1_TZK'
@@ -53,15 +77,20 @@ intrinsic_local = (np.asarray(img_res), fl_mm, np.asarray(sens_dim), (cx, cy))
 x = 1
 y = height
 dist_range = np.arange(1, 12, 1)
-vertices_bottom = np.array(list(itertools.product([x], [y], dist_range, [1])))
-vertices_bottom1 = np.array(list(itertools.product([x + 0.5], [y], dist_range, [1])))
-vertices_bottom2 = np.array(list(itertools.product([x - 0.5], [y], dist_range, [1])))
-vertices_up = np.array(list(itertools.product([x + 0.1], [y + 1.8], dist_range, [1])))
+grid_bottom = np.array(list(itertools.product([x], [y], dist_range, [1])))
+grid_bottom_r = np.array(list(itertools.product([x + 0.5], [y], dist_range, [1])))
+grid_bottom_l = np.array(list(itertools.product([x - 0.5], [y], dist_range, [1])))
+grid_up = np.array(list(itertools.product([x + 0.1], [y + 1.8], dist_range, [1])))
 
-vertices = np.vstack((vertices_bottom, vertices_up, vertices_bottom1, vertices_bottom2))
+grid = np.vstack((grid_bottom, grid_bottom_r, grid_bottom_l, grid_up))
+rw_system_grid = t3d.Handler3D(grid, operations=['rx'], k=intrinsic_local)
+rw_system_grid.transform(np.deg2rad(angle))
 
-rw_system = t3d.Handler3D(vertices, operations=['rx'], k=intrinsic_local)
-rw_system.transform(np.deg2rad(angle))
+object_distance = 8
+vertices_ob, faces = parse_3d_obj_file('scenes/test-obj.obj')
+rw_system = t3d.Handler3D(vertices_ob, operations=['s', 'ry', 't', 'rx'], k=intrinsic_local)
+rw_system.transform((True, np.asarray([0, 1.8, 0])), np.deg2rad(180), np.asarray([x, height, object_distance]),
+                            np.deg2rad(angle))  # Transform object in 3D space and project to image plane
 
 image = cv2.imread(image_path, 0)
 ud_image = cv2.undistort(image, intrinsic, dist)
@@ -69,5 +98,6 @@ clahe_adjust = cv2.createCLAHE(clipLimit=8, tileGridSize=(8, 8))
 ud_image = clahe_adjust.apply(ud_image)
 
 plt.imshow(ud_image, cmap='gray', vmin=0, vmax=255)
-plt.scatter(rw_system.img_points[:, 0], rw_system.img_points[:, 1], s=1, color='red')
+plt.scatter(rw_system.img_points[:, 0], rw_system.img_points[:, 1], s=0.5, color='red')
+plt.scatter(rw_system_grid.img_points[:, 0], rw_system_grid.img_points[:, 1], s=1, color='red')
 plt.show()
